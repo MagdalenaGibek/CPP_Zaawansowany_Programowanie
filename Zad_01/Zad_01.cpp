297e0c4f39c0504bcc1dfbd92ce3f056ab773b31
Magdalena Gibek <mgdln.los@gmail.com>
2022-08-30 18:33:00 +0200

    Zad_01 update

--- Zad_01/Zad_01.cpp
+++ Zad_01/Zad_01.cpp
@@ -2,14 +2,17 @@
 #include <string>
 #include <algorithm>
 
+std::string reverseStr(std::string str)
+{
+    reverse(str.begin(), str.end());
+    return str;
+}
+
 int main()
 {
-    std::string str;
-    std::string reverseStr;
+    std::string strToReverse;
     std::cout << "Wprowadź zwrot, który ma być zamieniony" << std::endl;
-    std::getline(std::cin >> std::ws, str);
-    reverse(str.begin(), str.end());
-
-    std::cout << "Zamieniony zwrot wygląda teraz tak:" << str << std::endl;
+    std::getline(std::cin >> std::ws, strToReverse);
+    std::cout << "Zamieniony zwrot wygląda teraz tak:" << reverseStr(strToReverse) << std::endl;
 }
 
