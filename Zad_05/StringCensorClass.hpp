e8f0a31e2746305d7165fcd1ed1a73480964279a
Magdalena Gibek <mgdln.los@gmail.com>
2022-09-01 11:19:33 +0200

    Zad_05 added

--- Zad_05/StringCensorClass.hpp
+++ Zad_05/StringCensorClass.hpp
@@ -1,3 +1,8 @@
+//Klasa: StringCensor
+//Napisz klasę która będzie cenzurować podane stringi(zamieniać wymagane litery na gwiazkę), operacje która ma wspierać ta klasa :
+//-addCensoredLetter(char c)
+//- std::string censor(std::string)
+
 #pragma once
 #include<string>
 #include<set>
